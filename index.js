e73dccd256441916c761e5d8f6ac61b6f4d0ab74
LazyLD <87400491+LazyLD@users.noreply.github.com>
2021-09-05 15:02:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var build = require('./src/build');
+var irregularsList = require('./src/irregulars-list');
+var isIrregular = require('./src/is-irregular');
+var isVerb = require('./src/is-verb');
+
+/**
+ * Conjugar qualquer verbo (regular ou irregular).
+ *
+ * @param {string} verb Um verbo qualquer.
+ * @returns {object} Toda as conjugações.
+ */
+module.exports = function conjugar(verb) {
+  if (!isVerb(verb)) {
+    throw new Error('Invalid verb');
+  }
+
+  return isIrregular(verb)
+    ? irregularsList[verb]
+    : build(verb);
+}
